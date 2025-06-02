167a4509132e8eb72ad2cb1ddb2efaf8c3ede715
Beniamin Mishkur <bmishkur@gmail.com>
2025-06-02 05:13:53 +0200

    hw push

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,45 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+  createPromise(delay, state)
+    .then(ms => {
+      iziToast.show({
+        title: '✅ Success',
+        message: `Fulfilled promise in ${ms}ms`,
+        position: 'topRight',
+        timeout: 3000,
+        icon: false,
+        class: 'fulfilled-toast'
+      });
+    })
+    .catch(ms => {
+      iziToast.show({
+        title: '❌ Error',
+        message: `Rejected promise in ${ms}ms`,
+        position: 'topRight',
+        timeout: 3000,
+        icon: false,
+        class: 'rejected-toast'
+      });
+    });
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
